22cd0171ac23fea1c1fd462a1279a479f50bc53c
puker <puker@puker.com>
2026-06-07 18:25:47 +0300

    update texts

--- cmd/root.go
+++ cmd/root.go
@@ -29,13 +29,8 @@ import (
 
 var rootCmd = &cobra.Command{
 	Use:   "haha",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A simple app to have haha",
+	Long:  "A simple app to have haha. Run 'haha' in the terminal to make a simple 'haha' happen.",
 	Run: func(cmd *cobra.Command, args []string) {
 		printHaha()
 	},
